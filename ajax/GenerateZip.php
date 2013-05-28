6a8eaf759e72d613ff737a5bb01a7bba6b77c634
Ritesh Patel <ritzpatel91@gmail.com>
2013-05-29 03:04:54 +0530

    Changes in zip functionality and slider for mobile devices.
    Zip functionality is working for large albums now.
    One can browse album images from mobile devices and can navigate through
    swipe.

--- ajax/GenerateZip.php
+++ ajax/GenerateZip.php
@@ -1,23 +1,27 @@
 <?php
-	$path_root="../../";
-    $path_app="../";
-    include_once($path_app."includes/fb-config.php");
-	$album_id = $_POST['albumid'];	 
-	$photos = $facebook->api("/{$album_id}/photos");
+	$path_root="../../";	// define path to root
+    $path_app="../";		// define path to app
+    include_once($path_app."includes/fb-config.php");	// include facebook config file
+	$album_id = $_POST['albumid'];	 	// get album id
+	$total_images=$_POST['total_images'];	 	//get totatl images in the album
+	$photos = $facebook->api("/{$album_id}/photos?limit=$total_images&offset=0");	
+	//build array to get source of album images.
 	$images=array();	
 	foreach($photos['data'] as $photo)
 	{
 		$images[]=$photo['source'];
 	}
 	$files = $images;
+
+	ini_set('max_execution_time', 0);	// set max_execution_time to make zip file even for large number of files.
 	//print_r($files);
-	$zipname = $path_app.'downloads/YourAlbum.zip';
-	$zip = new ZipArchive;
-	$zip->open($zipname, ZipArchive::OVERWRITE);
-	foreach ($files as $file) 
-	{
+	$zipname = $path_app.'downloads/YourAlbum.zip';		
+	$zip = new ZipArchive;		//create object of ZipArchive
+	$zip->open($zipname, ZipArchive::OVERWRITE);	
+	foreach ($files as $file) 		//build zip
+	{	  	
 	  	$file1=file_get_contents($file);
-	  	$zip->addFromString(basename($file),$file1);
+	  	$zip->addFromString(basename($file),$file1);	  	
 	}
 	$zip->close();
 	echo "1";
--- ajax/GetAlbumImages.php
+++ ajax/GetAlbumImages.php
@@ -1,13 +1,16 @@
 <?php
-	$path_root="../../";
-    $path_app="../";
-    include_once($path_app."includes/fb-config.php");
-	$album_id = $_POST['albumid'];	 
-	$photos = $facebook->api("/{$album_id}/photos"); 	
+	$path_root="../../";	// define path to root
+    $path_app="../";		// define path to app
+    include_once($path_app."includes/fb-config.php");	// include facebook config file
+	$album_id = $_POST['albumid'];		// get album id
+	$total_images=$_POST['total_images'];		//get totatl images in the album
+	$photos = $facebook->api("/{$album_id}/photos?limit=$total_images&offset=0"); 
+	//build array to get source of album images.	
 	$images=array();	
 	foreach($photos['data'] as $photo)
 	{	        
 	        $images[]=$photo['source'];
-	}
+	}	
+	// convert php array in to json data.
 	echo json_encode($images);
 ?>
