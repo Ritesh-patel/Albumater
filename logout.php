1fa23e5bf92d589a2f7e6df96625dd0010ad78d3
Ritesh Patel <ritzpatel91@gmail.com>
2013-05-30 02:15:47 +0530

    Change in logout page.
    Renamed  includes to lib.

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 	$path_root="../";
     $path_app="";
-    include_once($path_app."includes/fb-config.php");
+    include_once($path_app."lib/fb-config.php");
   	$facebook->destroySession();
   	setcookie('fbs_'.$facebook->getAppId(), '', time()-100, '/', $_SERVER['HTTP_HOST']."/rt-challenge/albumater");
   	session_destroy();  	
